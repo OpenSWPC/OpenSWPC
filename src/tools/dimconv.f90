c475d35404fb2ea1a5b478e1c2e9d54c8cb02be8
Takuto Maeda <tktmyd@hirosaki-u.ac.jp>
2023-01-29 11:23:37 +0900

    delete unused file

--- src/tools/dimconv.f90
+++
@@ -1,8 +0,0 @@
-!! ----------------------------------------------------------------------------------------------------------------------------- !!
-!>
-!! Convert 2D waveform output into pseudo-3D
-!!
-!! @copyright
-!!   Copyright 2021 Takuto Maeda. All rights reserved. This project is released under the MIT license.
-!<
-!! --
