e495133a1c546d639daf263ace138d851acb9b0a
Takuto Mada <tktmyd@hirosaki-u.ac.jp>
2024-09-01 15:15:02 +0900

    Simplified debug/assert/info routines

--- src/shared/m_debug.h
+++ src/shared/m_debug.h
@@ -1,36 +1,7 @@
-/*
-!! ------------------------------------------------------------------------- !!
-!!                                                          -*- mode:f90 -*-
-!! Header file for m_debug.F90
-!!
-!! ------------------------------------------------------------------------- !!
-*/
-
-#if defined(_DEBUG) || defined(DEBUG)
-#if defined(__INTEL_COMPILER)
-#define debug(a)   debug__macro( a, #a, __FILE__, __LINE__ )
-#define DEBUG(a)   debug__macro( a, #a, __FILE__, __LINE__ )
+#if defined(_DEBUG)
+#define debug(a)   debug__macro(a, __FILE__, __LINE__)
 #else              
-#define debug(a)   debug__macro( a, __FILE__, __LINE__ )
-#define DEBUG(a)   debug__macro( a, __FILE__, __LINE__ )
-#endif             
-#else              
-#define debug(a)   debug__void()
-#define DEBUG(a)   debug__void()
+#define debug(a)   debug__macro()
 #endif
-
-#if defined(_INFO) || defined(INFO)
-#define info(a)    info__macro( a, __FILE__, __LINE__ )
-#define INFO(a)    info__macro( a, __FILE__, __LINE__ )
-#else              
-#define info(a)    debug__void()
-#define INFO(a)    debug__void()
-#endif
-
-#if defined(__INTEL_COMPILER)
-#define assert(a)  assert__macro( a, #a, __FILE__, __LINE__ )
-#define ASSERT(a)  assert__macro( a, #a, __FILE__, __LINE__ )
-#else 
-#define assert(a)  assert__macro( a, __FILE__, __LINE__ )
-#define ASSERT(a)  assert__macro( a, __FILE__, __LINE__ )
-#endif             
+#define info(a)    info__macro(a, __FILE__, __LINE__)
+#define assert(a)  assert__macro(a, __FILE__, __LINE__)
